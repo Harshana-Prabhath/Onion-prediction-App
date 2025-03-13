fa18664d84006b53f8df56fb657b1268e10d526b
Harshana_P_Semasinghe <harshanaprabhathsemasinghe@gmail.com>
2025-03-13 12:53:01 +0530

    adding header and footer

--- src/Comonents/Header.tsx
+++ src/Comonents/Header.tsx
@@ -1,16 +1,104 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../styles/theme";
+import { Link } from "react-router";
 
 const HeaderWrapper = styled.div`
   background: ${theme.color.white};
   width: 100%;
-  height: 50px;
+  height: 60px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+const StyledNav = styled.nav`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  gap: 20px;
+  text-decoration: none;
+`;
+const ListItem = styled.li`
+  text-decoration: none;
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 20px;
+  color: ${theme.color.verdantGreen};
+  font-weight: 700;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -3px; /* Adjust position if needed */
+    width: 0;
+    height: 5px;
+    background-color: ${theme.color.springMeadow};
+    transition: width 0.3s ease-in-out;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
+`;
+const ExternalLink = styled.a`
+  text-decoration: none;
+  font-size: 20px;
+  color: ${theme.color.verdantGreen};
+  font-weight: 700;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -3px; /* Adjust position if needed */
+    width: 0;
+    height: 5px;
+    background-color: ${theme.color.springMeadow};
+    transition: width 0.3s ease-in-out;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
 `;
 
 const Header: React.FC = () => {
-  return <HeaderWrapper>This is a header</HeaderWrapper>;
+  const gitHubUrl = import.meta.env.VITE_GITHUB_URL;
+  return (
+    <HeaderWrapper>
+      <StyledNav>
+        <StyledList>
+          <ListItem>
+            <Link to="/">
+              <img
+                src="/src/assets/images/logo_onion.png"
+                alt="company-logo"
+                height={60}
+                width={60}
+              />
+            </Link>
+          </ListItem>
+          <ListItem>
+            <ExternalLink href={gitHubUrl} target="_blank">
+              Code Base
+            </ExternalLink>
+          </ListItem>
+          <ListItem>
+            <StyledLink style={{ textDecoration: "none" }} to="/about">
+              About Us
+            </StyledLink>
+          </ListItem>
+        </StyledList>
+      </StyledNav>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,5 +2,13 @@ export const theme = {
   color: {
     white: "#FFFFFF",
     red: "#222",
+    deepPurpleGray: "#54436B", // Muted dark purple with gray tones
+    mintGreen: "#50CB93", // Fresh green with a minty tone
+    neonMint: "#71EFA3", // Vibrant mint green
+    pastelGreen: "#ACFFAD", // Soft pastel green
+    verdantGreen: "#72BF78", // Rich natural green
+    springMeadow: "#A0D683", // Soft grassy green
+    lushLime: "#D3EE98", // Light lime green
+    sunnyGlow: "#FEFF9F", // Warm yellow with a soft glow
   },
 };
